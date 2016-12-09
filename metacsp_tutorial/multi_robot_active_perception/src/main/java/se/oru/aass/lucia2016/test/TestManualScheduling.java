0c4d71f86622b6e4679605723e219bff485e498b
Federico Pecora <federico.pecora@oru.se>
2016-12-09 18:46:19 +0100

    Getting there

--- metacsp_tutorial/multi_robot_active_perception/src/main/java/se/oru/aass/lucia2016/test/TestManualScheduling.java
+++ metacsp_tutorial/multi_robot_active_perception/src/main/java/se/oru/aass/lucia2016/test/TestManualScheduling.java
@@ -1,12 +1,20 @@
 package se.oru.aass.lucia2016.test;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.metacsp.framework.ConstraintNetwork;
+import org.metacsp.framework.Variable;
 import org.metacsp.multi.activity.ActivityNetworkSolver;
+import org.metacsp.multi.allenInterval.AllenInterval;
 import org.metacsp.multi.allenInterval.AllenIntervalConstraint;
+import org.metacsp.multi.spatial.DE9IM.DE9IMRelation;
+import org.metacsp.multi.spatial.DE9IM.GeometricShapeDomain;
+import org.metacsp.multi.spatial.DE9IM.GeometricShapeVariable;
 import org.metacsp.multi.spatioTemporal.paths.Pose;
+import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
 import org.metacsp.multi.spatioTemporal.paths.Trajectory;
 import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
 import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelopeSolver;
@@ -22,8 +30,15 @@ import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Subscriber;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+
 import se.oru.aass.lucia2016.execution.FlapForChaosDispatchingFunction;
 import se.oru.aass.lucia2016.meta.ViewCoordinator;
+import se.oru.aass.lucia2016.multi.ViewConstraintSolver;
 import se.oru.aass.lucia2016.utility.Convertor;
 import se.oru.aass.lucia2016.utility.FootPrintFactory;
 import se.oru.aass.lucia2016.utility.PathPlanFactory;
@@ -65,10 +80,11 @@ public class TestManualScheduling extends AbstractNodeMain {
 		}
 		
 		setupSolvers();
-		//setupMonitoring();
 		
 		//Exercises: see in this method
 		setupConstraintNetwork();
+		
+		setupMonitoring();
 	}
 	
 	private void setupMonitoring() {
@@ -92,74 +108,107 @@ public class TestManualScheduling extends AbstractNodeMain {
 
 	//Exercise method
 	private void setupConstraintNetwork() {
+		
+		//Hard coded poses in the Lucia map
 		Pose pose1 = new Pose(4.7579498291, 0.84562087059, 0.551774602413026);
 		Pose pose2 = new Pose(2.45750570297, 2.96492242813, -3.12842980123163);
 		
+		//Get paths from ROS so that turtlebot1 and turtlebot2
+		//can reach these poses, and create TrajectoryEnvelopes around them
 		TrajectoryEnvelope trajectoryEnvelopeRobot1 = createTrajectoryEnvelope(pose1, 1);
 		TrajectoryEnvelope trajectoryEnvelopeRobot2 = createTrajectoryEnvelope(pose2, 2);
 		
+		//Create a parking polygon for turtlebot1
 		TrajectoryEnvelope parkingRobot1 = (TrajectoryEnvelope)trajectoryEnvelopeSolver.createVariable("turtlebot1");
 		parkingRobot1.setFootprint(FootPrintFactory.getTurtlebotFootprint());
 		Trajectory parkingPoseRobot1 = new Trajectory(new Pose[] {trajectoryEnvelopeRobot1.getTrajectory().getPose()[0]});
 		parkingRobot1.setTrajectory(parkingPoseRobot1);
 		parkingRobot1.setRobotID(1);
-		parkingRobot1.getSymbolicVariableActivity().setSymbolicDomain("parking");
+		parkingRobot1.getSymbolicVariableActivity().setSymbolicDomain("Parking");
 
+		//Create a parking polygon for turtlebot2
 		TrajectoryEnvelope parkingRobot2 = (TrajectoryEnvelope)trajectoryEnvelopeSolver.createVariable("turtlebot2");
 		parkingRobot2.setFootprint(FootPrintFactory.getTurtlebotFootprint());
 		Trajectory parkingPoseRobot2 = new Trajectory(new Pose[] {trajectoryEnvelopeRobot2.getTrajectory().getPose()[0]});
 		parkingRobot2.setTrajectory(parkingPoseRobot2);
 		parkingRobot2.setRobotID(2);
-		parkingRobot2.getSymbolicVariableActivity().setSymbolicDomain("parking");
+		parkingRobot2.getSymbolicVariableActivity().setSymbolicDomain("Parking");
 
-		AllenIntervalConstraint parkingMeetsMoveRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Meets);
-		parkingMeetsMoveRobot1.setFrom(parkingRobot1);
-		parkingMeetsMoveRobot1.setTo(trajectoryEnvelopeRobot1);
-		trajectoryEnvelopeSolver.addConstraint(parkingMeetsMoveRobot1);
-		
-		AllenIntervalConstraint parkingMeetsMoveRobot2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Meets);
-		parkingMeetsMoveRobot2.setFrom(parkingRobot2);
-		parkingMeetsMoveRobot2.setTo(trajectoryEnvelopeRobot2);
-		trajectoryEnvelopeSolver.addConstraint(parkingMeetsMoveRobot2);
-
+		//turtlebot1 is parked from the origin of time
 		AllenIntervalConstraint releaseParkingRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN,ORIGIN));
 		releaseParkingRobot1.setFrom(parkingRobot1);
 		releaseParkingRobot1.setTo(parkingRobot1);
 		trajectoryEnvelopeSolver.addConstraint(releaseParkingRobot1);
-		
+
+		//turtlebot2 is parked from the origin of time
 		AllenIntervalConstraint releaseParkingRobot2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN,ORIGIN));
 		releaseParkingRobot2.setFrom(parkingRobot2);
 		releaseParkingRobot2.setTo(parkingRobot2);
 		trajectoryEnvelopeSolver.addConstraint(releaseParkingRobot2);
 
-		AllenIntervalConstraint releaseMoveRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN+25000,APSPSolver.INF));
+		//turtlebot1 should start moving immediately after parking (meets)
+		AllenIntervalConstraint parkingMeetsMoveRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Meets);
+		parkingMeetsMoveRobot1.setFrom(parkingRobot1);
+		parkingMeetsMoveRobot1.setTo(trajectoryEnvelopeRobot1);
+		trajectoryEnvelopeSolver.addConstraint(parkingMeetsMoveRobot1);
+
+		//turtlebot2 should start moving immediately after parking (meets)
+		AllenIntervalConstraint parkingMeetsMoveRobot2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Meets);
+		parkingMeetsMoveRobot2.setFrom(parkingRobot2);
+		parkingMeetsMoveRobot2.setTo(trajectoryEnvelopeRobot2);
+		trajectoryEnvelopeSolver.addConstraint(parkingMeetsMoveRobot2);
+
+		//Decide that turtlebot1 stays parked for at least 10 second
+		AllenIntervalConstraint releaseMoveRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN+10000,APSPSolver.INF));
 		releaseMoveRobot1.setFrom(trajectoryEnvelopeRobot1);
 		releaseMoveRobot1.setTo(trajectoryEnvelopeRobot1);
 		trajectoryEnvelopeSolver.addConstraint(releaseMoveRobot1);
-		
-		AllenIntervalConstraint releaseMoveRobot2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN+40000,APSPSolver.INF));
+
+		//Decide that turtlebot2 stays parked for at least 10 second
+		AllenIntervalConstraint releaseMoveRobot2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN+10000,APSPSolver.INF));
 		releaseMoveRobot2.setFrom(trajectoryEnvelopeRobot2);
 		releaseMoveRobot2.setTo(trajectoryEnvelopeRobot2);
 		trajectoryEnvelopeSolver.addConstraint(releaseMoveRobot2);
 
+
+//		//Decide that turtlebot1 should start moving at ORIGIN+25 seconds
+//		AllenIntervalConstraint releaseMoveRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN+25000,APSPSolver.INF));
+//		releaseMoveRobot1.setFrom(trajectoryEnvelopeRobot1);
+//		releaseMoveRobot1.setTo(trajectoryEnvelopeRobot1);
+//		trajectoryEnvelopeSolver.addConstraint(releaseMoveRobot1);
+//
+//		//Decide that turtlebot2 should start moving at ORIGIN+40 seconds
+//		AllenIntervalConstraint releaseMoveRobot2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(ORIGIN+40000,APSPSolver.INF));
+//		releaseMoveRobot2.setFrom(trajectoryEnvelopeRobot2);
+//		releaseMoveRobot2.setTo(trajectoryEnvelopeRobot2);
+//		trajectoryEnvelopeSolver.addConstraint(releaseMoveRobot2);
+
+		//Printing all the spatial relations between two trajectory envelopes
+		for (DE9IMRelation.Type t : DE9IMRelation.getRelations(trajectoryEnvelopeRobot1.getEnvelopeVariable(), trajectoryEnvelopeRobot2.getEnvelopeVariable())) {
+			System.out.println("Possible spatial relation: " + t);
+		}
+		
+		//Print the temporal domains
+		long earliestStrartTimeRobot1 = trajectoryEnvelopeRobot1.getTemporalVariable().getEST();
+		long earliestEndTimeRobot1 = trajectoryEnvelopeRobot1.getTemporalVariable().getEET();
+		long earliestStrartTimeRobot2 = trajectoryEnvelopeRobot2.getTemporalVariable().getEST();
+		long earliestEndTimeRobot2 = trajectoryEnvelopeRobot2.getTemporalVariable().getEET();
+		
+		System.out.println("(EST,EET) Robot 1: (" + earliestStrartTimeRobot1 + "," + earliestEndTimeRobot1 + ")");
+		System.out.println("(EST,EET) Robot 2: (" + earliestStrartTimeRobot2 + "," + earliestEndTimeRobot2 + ")");
+
+//		refineTrajectoryEnvelopes(trajectoryEnvelopeRobot1, trajectoryEnvelopeRobot2);
+		
+//		//Make turtlebot2 goes before turtlebot1
+//		AllenIntervalConstraint robot2BeforeRobot1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(1,APSPSolver.INF));
+//		robot2BeforeRobot1.setFrom(trajectoryEnvelopeRobot2);
+//		robot2BeforeRobot1.setTo(trajectoryEnvelopeRobot1);
+//		trajectoryEnvelopeSolver.addConstraint(robot2BeforeRobot1);
 		
 		//Visualize everything
 		TrajectoryEnvelopeAnimator tea = new TrajectoryEnvelopeAnimator("Test");
 		tea.addTrajectoryEnvelopes(parkingRobot1, parkingRobot2, trajectoryEnvelopeRobot1, trajectoryEnvelopeRobot2);
 
-		// TODO code here
-	
-//		AllenIntervalConstraint overlap = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Overlaps, new Bounds(5000, APSPSolver.INF));
-//		overlap.setFrom(trajectoryEnvelope1);
-//		overlap.setTo(trajectoryEnvelope2);
-//		trajectoryEnvelopeSolver.addConstraint(overlap);
-		
-		
-//		//Printing all the spatial relations between two trajectory envelopes
-//		for (DE9IMRelation.Type t : DE9IMRelation.getRelations(trajectoryEnvelope1.getEnvelopeVariable(), trajectoryEnvelope2.getEnvelopeVariable())) {
-//			System.out.println(t);
-//		}
-		
 //		TimelinePublisher tp = new TimelinePublisher(ans.getConstraintNetwork(), new Bounds(0,60000), true, "Time", "turtlebot1", "turtlebot2", "turtlebot3");
 //		TimelineVisualizer tv = new TimelineVisualizer(tp);
 //		tv.startAutomaticUpdate(CONTROL_PERIOD);
@@ -232,7 +281,236 @@ public class TestManualScheduling extends AbstractNodeMain {
 		}, 10);	
 	}
 
+	private ConstraintNetwork refineTrajectoryEnvelopes(TrajectoryEnvelope var1, TrajectoryEnvelope var2) {
+		
+		int MINIMUM_SIZE = 5;
+		
+		ConstraintNetwork toReturn = new ConstraintNetwork(null);
+		GeometryFactory gf = new GeometryFactory();
+		Geometry se1 = ((GeometricShapeDomain)var1.getEnvelopeVariable().getDomain()).getGeometry();
+		Geometry se2 = ((GeometricShapeDomain)var2.getEnvelopeVariable().getDomain()).getGeometry();
+		Geometry intersectionse1se2 = se1.intersection(se2);
 
+		boolean useDefaultEnvelopeChunks = false;
+
+		if (!intersectionse1se2.isValid()) {
+			intersectionse1se2 = intersectionse1se2.symDifference(intersectionse1se2.getBoundary());
+			metaCSPLogger.info("Intersection " + var1 + " with " + var2 + " invalid - fixing");
+		}
+
+		if (intersectionse1se2 instanceof MultiPolygon) {
+			metaCSPLogger.info("Intersection " + var1 + " with " + var2 + " too complex - skipping");
+			useDefaultEnvelopeChunks = true;
+			//return toReturn;								
+		}
+
+		boolean in  = false;
+		int countIn = 0;
+		for (int i = 0; i < var1.getPathLength(); i++) {
+			Coordinate coord = var1.getTrajectory().getPositions()[i];
+			Point point = gf.createPoint(coord);
+			if (intersectionse1se2.contains(point) && !in) {
+				in = true;
+				if (++countIn > 1) {
+					metaCSPLogger.info("Reference path of " + var1 + " enters intersection with " + var2 + " multiple times - skipping");
+					useDefaultEnvelopeChunks = true;
+					break;
+					//return toReturn;					
+				}
+			}
+			if (!intersectionse1se2.contains(point)) {
+				in = false;
+			}
+		}
+
+		double areaDifference = intersectionse1se2.symDifference(intersectionse1se2.getBoundary()).union(se1).getArea()-se1.getArea();
+		if (areaDifference > 0.001) {
+			metaCSPLogger.info("Intersection " + var1 + " with " + var2 + " seems corrupt (area increased by " + areaDifference + ") - skipping ");
+			useDefaultEnvelopeChunks = true;
+			//return toReturn;											
+		}
+
+		// IRAN: UNOCMMENT THIS IF YOU HAVE PROBLEMS WITH SCHEDULING
+		//		if (!intersectionse1se2.coveredBy(se1)) {
+		//			logger.info("Intersection " + var1 + " with " + var2 + " is corrupted - skipping");
+		//			return toReturn;											
+		//		}
+
+		//		logger.info("Refining " + var1 + " with " + var2);
+
+		ArrayList<PoseSteering> var1sec1 = new ArrayList<PoseSteering>();
+		ArrayList<PoseSteering> var1sec2 = new ArrayList<PoseSteering>();
+		ArrayList<PoseSteering> var1sec3 = new ArrayList<PoseSteering>();
+
+		boolean skipSec1 = false;
+		boolean skipSec3 = false;
+
+		if (useDefaultEnvelopeChunks) {
+			float percentageChunckOne = 0.30f;
+			float percentageChunckTwo = 0.40f;
+			for (int i = 0; i < var1.getPathLength(); i++) {
+				PoseSteering ps = var1.getTrajectory().getPoseSteering()[i];
+				if (i < var1.getPathLength()*percentageChunckOne) var1sec1.add(ps);
+				else if (i < var1.getPathLength()*(percentageChunckOne+percentageChunckTwo)) var1sec2.add(ps);
+				else var1sec3.add(ps);
+			}
+			metaCSPLogger.info("Using default chunk sizes " + var1sec1.size() + " / " + var1sec2.size() + " / " + var1sec3.size());
+		}
+		else {	
+			for (int i = 0; i < var1.getPathLength(); i++) {
+				Coordinate coord = var1.getTrajectory().getPositions()[i];
+				PoseSteering ps = var1.getTrajectory().getPoseSteering()[i];
+				Point point = gf.createPoint(coord);
+				Geometry fp = var1.makeFootprint(ps);
+				if (!intersectionse1se2.intersects(fp) && var1sec2.isEmpty()) {
+					var1sec1.add(ps);
+				}
+				else if (intersectionse1se2.intersects(fp)) {
+					var1sec2.add(ps);
+				}
+				else if (!intersectionse1se2.intersects(fp) && !var1sec2.isEmpty()) {
+					var1sec3.add(ps);
+				}
+			}
+
+			//Add to start
+			boolean done = false;
+			while (!done) {
+				try {
+					Geometry lastPolySec1 = var1.makeFootprint(var1sec1.get(var1sec1.size()-1));
+					if (lastPolySec1.disjoint(se2)) done = true;
+					else {
+						var1sec2.add(0,var1sec1.get(var1sec1.size()-1));
+						var1sec1.remove(var1sec1.size()-1);
+						metaCSPLogger.info("Added to start... (1)");
+					}
+				} catch (IndexOutOfBoundsException e) 
+				{ skipSec1 = true; done = true; }
+			}
+			//If sec1 emptied, remove it
+			if (var1sec1.size() < MINIMUM_SIZE) {
+				while (var1sec1.size() > 0) {
+					var1sec2.add(0,var1sec1.get(var1sec1.size()-1));
+					var1sec1.remove(var1sec1.size()-1);
+				}
+				skipSec1 = true;
+			}
+
+			//Add to end
+			done = false;
+			while (!done) {
+				try {
+					Geometry firstPolySec3 = var1.makeFootprint(var1sec3.get(0));
+					if (firstPolySec3.disjoint(se2)) done = true;
+					else {
+						var1sec2.add(var1sec3.get(0));
+						var1sec3.remove(0);
+						//					logger.info("Added to end... (1)");
+					}
+				} catch (IndexOutOfBoundsException e) { skipSec3 = true; done = true; }
+			}
+			//If sec3 emptied, remove it
+			if (var1sec3.size() < MINIMUM_SIZE) {
+				while (var1sec3.size() > 0) {
+					var1sec2.add(var1sec3.get(0));
+					var1sec3.remove(0);
+				}
+				skipSec3 = true;
+			}
+
+			if (var1sec2.size() < MINIMUM_SIZE) {
+				if (var1sec1.size() > MINIMUM_SIZE) {
+					var1sec2.add(0,var1sec1.get(var1sec1.size()-1));
+					var1sec1.remove(var1sec1.size()-1);
+					//				logger.info("Added to start... (2)");
+				}
+				else if (var1sec3.size() > MINIMUM_SIZE) {
+					var1sec2.add(var1sec3.get(0));
+					var1sec3.remove(0);				
+					//				logger.info("Added to end... (2)");
+				}
+			}
+
+			if ((skipSec1 && skipSec3) || (!skipSec1 && var1sec1.size() < MINIMUM_SIZE) || (!skipSec3 && var1sec3.size() < MINIMUM_SIZE) || var1sec2.size() < MINIMUM_SIZE) {
+				metaCSPLogger.fine("Intersection " + var1 + " with " + var2 + " too small - skipping");
+				return toReturn;
+			}
+
+		}
+
+		var1.setRefinable(false);
+		ArrayList<Trajectory> newTrajectories = new ArrayList<Trajectory>();
+		ArrayList<TrajectoryEnvelope> newTrajectoryEnvelopes = new ArrayList<TrajectoryEnvelope>();
+
+		if (!skipSec1) {
+			newTrajectories.add(new Trajectory(var1sec1.toArray(new PoseSteering[var1sec1.size()]),var1.getTrajectory().getDts(0, var1sec1.size())));
+			newTrajectories.add(new Trajectory(var1sec2.toArray(new PoseSteering[var1sec2.size()]),var1.getTrajectory().getDts(var1sec1.size(), var1sec1.size()+var1sec2.size())));
+			if (!skipSec3) {
+				newTrajectories.add(new Trajectory(var1sec3.toArray(new PoseSteering[var1sec3.size()]),var1.getTrajectory().getDts(var1sec1.size()+var1sec2.size(),var1.getTrajectory().getPoseSteering().length)));
+			}
+		}
+		else {
+			newTrajectories.add(new Trajectory(var1sec2.toArray(new PoseSteering[var1sec2.size()]),var1.getTrajectory().getDts(0, var1sec2.size())));
+			if (!skipSec3) {
+				newTrajectories.add(new Trajectory(var1sec3.toArray(new PoseSteering[var1sec3.size()]),var1.getTrajectory().getDts(var1sec2.size(),var1.getTrajectory().getPoseSteering().length)));
+			}			
+		}
+
+		Variable[] newVars = trajectoryEnvelopeSolver.createVariables(newTrajectories.size());
+		for (int i = 0; i < newVars.length; i++) {
+			TrajectoryEnvelope te = (TrajectoryEnvelope)newVars[i];
+			//te.setFootprint(var1.getWidth(), var1.getLength(), var1.getDeltaW(), var1.getDeltaL());
+			te.setFootprint(var1.getFootprint());
+			//Only for second!
+			//			if ((!skipSec1 && i == 1) || (skipSec1 && i == 0)) {
+			//				te.setRefinable(false);
+			//				refinedWith.get(geometricShapeVariable).add(te);
+			//			}
+			//			System.out.println("doing i = " + i + " skipsec1: " + skipSec1 + " skipsec3: " + skipSec3);
+			te.setTrajectory(newTrajectories.get(i));
+			te.setSuperEnvelope(var1);
+			te.setRobotID(var1.getRobotID());
+			var1.addSubEnvelope(te);
+			newTrajectoryEnvelopes.add(te);			
+		}
+
+		AllenIntervalConstraint starts = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Starts);
+		starts.setFrom(newTrajectoryEnvelopes.get(0));
+		starts.setTo(var1);
+		toReturn.addConstraint(starts);
+
+		AllenIntervalConstraint finishes = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Finishes);
+		finishes.setFrom(newTrajectoryEnvelopes.get(newTrajectoryEnvelopes.size()-1));
+		finishes.setTo(var1);
+		toReturn.addConstraint(finishes);
+
+		double minTTT12 = 0.0;
+
+		if (!skipSec1) minTTT12 = var1.getTrajectory().getDTs()[var1sec1.size()];
+		else minTTT12 = var1.getTrajectory().getDTs()[var1sec2.size()];
+		long minTimeToTransition12 = (long)(TrajectoryEnvelope.RESOLUTION*minTTT12);
+		AllenIntervalConstraint before1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(minTimeToTransition12,minTimeToTransition12));
+		before1.setFrom(newTrajectoryEnvelopes.get(0));
+		before1.setTo(newTrajectoryEnvelopes.get(1));
+		toReturn.addConstraint(before1);
+
+		if (newTrajectoryEnvelopes.size() > 2) {
+			double minTTT23 = var1.getTrajectory().getDTs()[var1sec1.size()+var1sec2.size()];
+			long minTimeToTransition23 = (long)(TrajectoryEnvelope.RESOLUTION*minTTT23);
+			AllenIntervalConstraint before2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(minTimeToTransition23,minTimeToTransition23));
+			before2.setFrom(newTrajectoryEnvelopes.get(1));
+			before2.setTo(newTrajectoryEnvelopes.get(2));
+			toReturn.addConstraint(before2);
+		}
+
+		//		System.out.println("var1sec1 (" + skipSec1 + "): " + var1sec1);
+		//		System.out.println("var1sec2: " + var1sec2);
+		//		System.out.println("var1sec3 (" + skipSec3 + "): " + var1sec3);
+		//		System.out.println("DTs of var1sec2: " + Arrays.toString(var1.getTrajectory().getDts( var1sec2.size(),var1.getTrajectory().getDTs().length-1 )));
+		trajectoryEnvelopeSolver.addConstraints(toReturn.getConstraints());
+
+		return toReturn;
+	}
 
 
 }
